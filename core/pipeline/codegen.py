e4624eaccf75c0b235db4c02b776c7ba95ccb63f
Legendyboi <legendyboi.dev@proton.me>
2024-06-26 16:25:51 +0530

    Added Proper Support for Printing Strings and Numbers

--- core/pipeline/codegen.py
+++ core/pipeline/codegen.py
@@ -71,27 +71,90 @@ class CodeGenVisitor:
             return self.builder.global_string_ptr(node.value)
 
     def visit_PrintStmtNode(self, node):
-        var_ptr = self.variables.get(node.identifier)
-        if var_ptr is None:
-            raise Exception(f"Undefined variable: {node.identifier}")
-        var_val = self.builder.load(var_ptr, node.identifier)
+        if isinstance(node.identifier, int):
+            print("its int!")
+            print(node.identifier)
 
-        # Declare the printf function
-        voidptr_ty = ir.IntType(8).as_pointer()
-        printf_ty = ir.FunctionType(ir.IntType(32), [voidptr_ty], var_arg=True)
-        printf = ir.Function(self.module, printf_ty, name="printf")
+            int_val = ir.Constant(ir.IntType(32), node.identifier)
 
-        # Create format string
-        format_str = "%d\n\0"
-        c_format_str = ir.Constant(ir.ArrayType(ir.IntType(8), len(format_str)),
-                                   bytearray(format_str.encode("utf8")))
-        global_format_str = ir.GlobalVariable(self.module, c_format_str.type, name="fstr")
-        global_format_str.linkage = 'internal'
-        global_format_str.global_constant = True
-        global_format_str.initializer = c_format_str
+            # Declare the printf function
+            voidptr_ty = ir.IntType(8).as_pointer()
+            printf_ty = ir.FunctionType(ir.IntType(32), [voidptr_ty], var_arg=True)
+            printf = ir.Function(self.module, printf_ty, name="printf")
 
-        # Cast the format string to i8*
-        fmt_arg = self.builder.bitcast(global_format_str, voidptr_ty)
-        self.builder.call(printf, [fmt_arg, var_val])
+            # Create format string
+            format_str = "%d\n\0"
+            c_format_str = ir.Constant(ir.ArrayType(ir.IntType(8), len(format_str)),
+                                    bytearray(format_str.encode("utf8")))
+            global_format_str = ir.GlobalVariable(self.module, c_format_str.type, name="fstr")
+            global_format_str.linkage = 'internal'
+            global_format_str.global_constant = True
+            global_format_str.initializer = c_format_str
+
+            # Cast the format string to i8*
+            fmt_arg = self.builder.bitcast(global_format_str, voidptr_ty)
+            self.builder.call(printf, [fmt_arg, int_val])
+
+        elif '"' in node.identifier and isinstance(node.identifier, str):
+            print("its str!")
+            print(f"Heres the String Value Obtained: {node.identifier}")
+            string_val = node.identifier.strip("\"")
+            print(f"Heres the String Value after Strip: {string_val}")
+
+            string_val += "\0"
+            c_str_val = ir.Constant(ir.ArrayType(ir.IntType(8), len(string_val)),
+                                    bytearray(string_val.encode("utf8")))
+            print(f"c_str_val: {c_str_val}")
+
+            # Declare the printf function
+            voidptr_ty = ir.IntType(8).as_pointer()
+            printf_ty = ir.FunctionType(ir.IntType(32), [voidptr_ty], var_arg=True)
+            printf = ir.Function(self.module, printf_ty, name="printf")
+
+            c_str = self.builder.alloca(c_str_val.type)
+            self.builder.store(c_str_val, c_str)
+            print(f"c_str: {c_str}")
+
+            # Create format string
+            format_str = "%s\n\0"
+            c_format_str = ir.Constant(ir.ArrayType(ir.IntType(8), len(format_str)),
+                                    bytearray(format_str.encode("utf8")))
+            print(f"c_format_str: {c_format_str}")
+            global_format_str = ir.GlobalVariable(self.module, c_format_str.type, name="fstr")
+            global_format_str.linkage = 'internal'
+            global_format_str.global_constant = True
+            global_format_str.initializer = c_format_str
+
+            fmt_arg = self.builder.bitcast(global_format_str, voidptr_ty)
+            print(fmt_arg)
+            self.builder.call(printf, [fmt_arg, c_str])
+            
+
+        else:
+            var_ptr = self.variables.get(node.identifier)
+            print(f"identifier: {node.identifier}")
+            print(f"heres the var_ptr: {var_ptr}")
+            if var_ptr is None:
+                raise Exception(f"Undefined variable: {node.identifier}")
+            var_val = self.builder.load(var_ptr, node.identifier)
+            print(f"heres the var_val: {var_val}")
+
+            # Declare the printf function
+            voidptr_ty = ir.IntType(8).as_pointer()
+            printf_ty = ir.FunctionType(ir.IntType(32), [voidptr_ty], var_arg=True)
+            printf = ir.Function(self.module, printf_ty, name="printf")
+
+            # Create format string
+            format_str = "%d\n\0"
+            c_format_str = ir.Constant(ir.ArrayType(ir.IntType(8), len(format_str)),
+                                    bytearray(format_str.encode("utf8")))
+            global_format_str = ir.GlobalVariable(self.module, c_format_str.type, name="fstr")
+            global_format_str.linkage = 'internal'
+            global_format_str.global_constant = True
+            global_format_str.initializer = c_format_str
+
+            # Cast the format string to i8*
+            fmt_arg = self.builder.bitcast(global_format_str, voidptr_ty)
+            self.builder.call(printf, [fmt_arg, var_val])
 
 
